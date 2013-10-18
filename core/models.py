31ad46aa6073befc29ef3f827ea671ce6bdb8d7f
Markevych Alexander <rabota.pmr@gmail.com>
2013-10-18 23:46:20 +0300

    Try to share models for admin

--- core/models.py
+++ core/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 class Servers(models.Model):
-	name = models.CharField(max_length = 100, blank = False)
-	ip = models.CharField(max_length = 15)
+    name = models.CharField(max_length = 100, blank = False)
+    ip = models.CharField(max_length = 15)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
-class Client(models.Model):
-	server_id = models.ForeignKey(Servers)
-	name = models.CharField(max_length=100, blank = False)
-	active = models.BooleanField(default = False)
-
+class Clients(models.Model):
+    server_id = models.ForeignKey(Servers)
+    name = models.CharField(max_length=100, blank = False)
+    active = models.BooleanField(default = False)
