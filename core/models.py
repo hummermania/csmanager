fb9ffad62e9855d2118faac4b8bc9a849f08dfc3
Markevych Alexander <rabota.pmr@gmail.com>
2013-10-19 11:52:52 +0300

    Add custom error messages for validation when server must contain unique client names

--- core/models.py
+++ core/models.py
@@ -1,19 +1,44 @@
 from django.db import models
 from django.contrib import admin
+from django.core.exceptions import ValidationError, NON_FIELD_ERRORS
 
 # Create your models here.
 class Server(models.Model):
     name = models.CharField(max_length = 100, blank = False)
     ip = models.CharField(max_length = 15)
 
-    def __str__(self):
-        return self.name + ", ipv4 = " + self.ip
+    def __unicode__(self):
+        return u'%s, ipv4 = %s' % (self.name, self.ip)
 
-# Create your models here.
+
 class Client(models.Model):
     server_id = models.ForeignKey(Server)
     name = models.CharField(max_length=100, blank = False)
     active = models.BooleanField(default = True)
     
-    def __str__(self):
+    def __unicode__(self):
+        #TODO: Add Server name before client name
         return self.name
+
+
+    def validate_unique(self, exclude=None):
+        super(Client, self).validate_unique()
+        query_set = Client.objects.filter(name=self.name)
+        if query_set.filter(server_id=self.server_id).exists():
+            raise ValidationError(
+                {
+                    NON_FIELD_ERRORS:
+                    ('Client name must be unique per Server!',)
+                }
+            )
+
+    def save(self, *args, **kwargs):
+
+        self.validate_unique()
+
+        super(Client, self).save(*args, **kwargs)
+    
+    # If we use unique_together it override "validate_unique" on the low level
+    # and don't allow to raise custom error message
+    #class Meta:
+        #unique_together = ("server_id", "name")
