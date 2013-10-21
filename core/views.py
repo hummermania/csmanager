13dbb5d9d6bbc7b69eea7cbeb106e6ddf3958016
Маркевич Александр <a.markevich@paynet.pmr>
2013-10-21 10:00:21 +0300

    Correct the code indentation

--- core/views.py
+++ core/views.py
@@ -9,35 +9,35 @@ from core.models import Client
 
 
 class ClientForm(ModelForm):
-	class Meta:
-		model = Client
-		#fields = ("pk","server_id","name","active")
+    class Meta:
+        model = Client
+        #fields = ("pk","server_id","name","active")
 
 @render_to('home.html')
 def home_page(request):
-	page_title = 'Django app.'
-	return {'page_title': page_title}
+    page_title = 'Django app.'
+    return {'page_title': page_title}
 
 
 @render_to('clients.html')
 def client_index(request):
-	clients = Client.objects.all()
-	return {'clients': clients}
+    clients = Client.objects.all()
+    return {'clients': clients}
 
 
 @render_to('clients.html')
 def client_details(request, client_id):
-	# TODO: Find why boolean field "active" not save in request
-	if request.method == 'POST':
-		form = ClientForm(request.POST)
-		if form.is_valid():
-			new_client = form.save(commit = False)
-			new_client.id = client_id
-			new_client.save()
-		return redirect('client_index')
+    # TODO: Find why boolean field "active" not save in request
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            new_client = form.save(commit = False)
+            new_client.id = client_id
+            new_client.save()
+        return redirect('client_index')
 
-	else:
-		client = get_object_or_404(Client, pk = client_id)
-		form = ClientForm(instance = client)
-		return {'form' : form }	
+    else:
+        client = get_object_or_404(Client, pk = client_id)
+        form = ClientForm(instance = client)
+        return {'form' : form } 
 
