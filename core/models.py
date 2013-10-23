fa34d053b38b7f7177becb263f4d2effef3a9474
Markevych Alexander <rabota.pmr@gmail.com>
2013-10-23 18:39:08 +0300

    Add the complex index on Server model. One server may have some different IP-address, and all IP-address is unique

--- core/models.py
+++ core/models.py
@@ -1,25 +1,15 @@
-import logging
-
 from django.db import models
 from django.contrib import admin
 
-
-
-logger = logging.getLogger(__name__)
-
-YES_NO = (
-    ( 1 , 'Yes'),
-    ( 0 , 'No'),
-)
-
-
-# Create your models here.
 class Server(models.Model):
-    name = models.CharField(max_length = 100, blank = False, unique= True)
+    name = models.CharField(max_length = 100, blank = False)
     ip = models.GenericIPAddressField(protocol = 'both', unique= True)
 
     def __unicode__(self):
         return u'%s, ipv4 = %s' % (self.name, self.ip)
+    
+    class Meta:
+        unique_together = ("name", "ip")
 
 
 class Client(models.Model):
